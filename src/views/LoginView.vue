e8c2babffe35ddabfe1bc4473b863f0d14d72040
Audy-Wallace <audywallace@DESKTOP-UL64QLD.localdomain>
2022-05-24 20:57:57 +0700

    login register template

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,58 @@
 <script>
-export default{
-    name: "LoginView"
-}
+export default {
+  name: "LoginView",
+  data(){
+      return {
+          formObject: {
+              email:"",
+              password:"",
+          }
+      }
+  }
+};
 </script>
-<template></template>
+<template>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col">
+      <div class="text-center">
+        <h1 class="text-5xl font-bold">Login now!</h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+      </div>
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <form>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                class="input input-bordered"
+                v-model="formObject.email"
+              />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                class="input input-bordered"
+                v-model="formObject.password"
+              />
+            </div>
+            <div class="form-control mt-6">
+              <button class="btn">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+  name: "RegisterView",
+  data() {
+    return {
+      formObject: {
+        email: "",
+        displayName: "",
+        password: "",
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <!-- component -->
+  <div class="bg-base-200 min-h-screen flex flex-col">
+    <div
+      class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2"
+    >
+      <div class="bg-white px-6 py-8 rounded-xl shadow-xl text-black w-full">
+        <h1 class="mb-8 text-3xl text-center">Sign up</h1>
+        <input
+          type="text"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="displayName"
+          placeholder="Display Name"
+        />
+
+        <input
+          type="text"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="email"
+          placeholder="Email"
+        />
+
+        <input
+          type="password"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="password"
+          placeholder="Password"
+        />
+        <input
+          type="password"
+          class="block border border-grey-light w-full p-3 rounded mb-4"
+          name="confirm_password"
+          placeholder="Confirm Password"
+        />
+        <div class = "form-control">
+            <button
+              type="submit"
+              class="btn"
+            >
+              Create Account
+            </button>
+        </div>
+      <div class="text-grey-dark mt-6 text-center">
+        Already have an account?
+        <a class="no-underline border-b border-blue text-blue" href="../login/">
+          Log in </a
+        >.
+      </div>
+      </div>
+
+    </div>
+  </div>
+</template>
