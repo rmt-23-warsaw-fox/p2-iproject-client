7505bd27223a8e1e4aeca5695964e1cb6ca0007a
Audy-Wallace <audywallace@DESKTOP-UL64QLD.localdomain>
2022-05-24 20:14:09 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script>
+import { RouterView } from "vue-router";
+</script>
+<template>
+<RouterView/>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,6 @@
+<script>
+export default{
+    name: "LoginView"
+}
+</script>
+<template></template>
