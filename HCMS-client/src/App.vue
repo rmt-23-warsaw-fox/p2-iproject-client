bf22acfaa394a7c5d2748d5af220891a4cda568b
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2022-05-24 10:19:39 +0700

    inital commit: template application

---
+++ HCMS-client/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { RouterView } from "vue-router";
+import NavBar from "./components/NavBar.vue";
+export default {
+  components: {
+    RouterView,
+    NavBar,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <NavBar />
+    <RouterView />
+  </div>
+</template>
