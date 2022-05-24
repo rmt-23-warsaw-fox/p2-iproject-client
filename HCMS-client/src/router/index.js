550a1084df5eedcd202355faa04581eec92d1246
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2022-05-25 03:18:11 +0700

    added symptomp checker feature

--- HCMS-client/src/router/index.js
+++ HCMS-client/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../views/HomePage.vue";
 import DiagnosePage from "../views/DiagnosePage.vue";
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
