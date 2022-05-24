7ce6c189e31883df7e269430c9814c664a50a2ea
redyRenno <redyrenno@gmail.com>
2022-05-24 16:41:45 +0900

    add component - navbar - home

---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,48 @@
+<script>
+
+</script>
+
+<template>
+  <div class="full-screen-height bg-light">
+    <div class="container container-center border border-dark">
+      <div>
+        <div class="row mt-4">
+          <div class="col d-grid gap-3">
+            <div class="card  width-400">
+              <h5 class="card-header">Faldo - 24 minute ago</h5>
+              <div class="card-body">
+                <img src="https://qph.fs.quoracdn.net/main-qimg-c7a176cb5bcaec3f681b2b69b2e6f158-lq" alt=""
+                  style="width: 100%" />
+
+                <p class="card-text">That me wkwkwk</p>
+                <a href="#" class="btn btn-primary">Like</a>
+              </div>
+            </div>
+            <div class="card width-400">
+              <h5 class="card-header">Faldo - 24 minute ago</h5>
+              <div class="card-body">
+                <img src="https://qph.fs.quoracdn.net/main-qimg-c7a176cb5bcaec3f681b2b69b2e6f158-lq" alt=""
+                  style="width: 100%" />
+
+                <p class="card-text">That me wkwkwk</p>
+                <a href="#" class="btn btn-primary">Like</a>
+              </div>
+            </div>
+          </div>
+          <div class="col border">
+            Chat
+            <ul class="list-group">
+              <li class="list-group-item">An item</li>
+              <li class="list-group-item">A second item</li>
+              <li class="list-group-item">A third item</li>
+              <li class="list-group-item">A fourth item</li>
+              <li class="list-group-item">And a fifth one</li>
+            </ul>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  </div>
+</template>
