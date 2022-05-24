882fe9042cb32160a46ddd9f918c0c6badfa6932
Edgar Dimas Isaadrazak <edgar.dimas.ir@gmail.com>
2022-05-24 14:45:05 +0700

    counter modify

--- HCMS-client/src/stores/counter.js
+++ HCMS-client/src/stores/counter.js
@@ -13,8 +13,52 @@ export const useCounterStore = defineStore({
     increment() {
       this.counter++;
     },
-    diagnose(){
-      
+    async getAilments(input) {
+      console.log(input, "input get ailment <<<<<");
+      if (input.symptomp1 === "") {
+        Swal.fire({
+          title: "Error!",
+          text: "first symptomp must be filled",
+          icon: "error",
+          confirmButtonText: "Ok",
+        });
+      } else {
+        const options = {
+          method: "GET",
+          url: "https://priaid-symptom-checker-v1.p.rapidapi.com/diagnosis",
+          params: {
+            gender: "male",
+            year_of_birth: "1984",
+            symptoms: `[${input.symptomp1},${input.symptomp2},${input.symptomp3}]`,
+            language: "en-gb",
+          },
+          headers: {
+            "X-RapidAPI-Host": "priaid-symptom-checker-v1.p.rapidapi.com",
+            "X-RapidAPI-Key": import.meta.env.VITE_API_SECRET,
+          },
+        };
+        try {
+          const response = await axios.request(options);
+          console.log(response, "<<<< response store");
+          if (response.data.length === 0) {
+            Swal.fire({
+              title: "Error!",
+              text: "Sorry, we cannot find your ailments",
+              icon: "error",
+              confirmButtonText: "Ok",
+            });
+          }
+          this.ailments = response;
+        } catch (err) {
+          console.log(err);
+          Swal.fire({
+            title: "Error!",
+            text: "Sorry, we cannot find your ailments",
+            icon: "error",
+            confirmButtonText: "Ok",
+          });
+        }
+      }
     }
   },
 });
