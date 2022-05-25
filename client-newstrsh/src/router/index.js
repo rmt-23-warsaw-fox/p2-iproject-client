cc982419f395e86b5be25f0810027d777ab0dc62
Romzi Fauzi <romziflubis@gmail.com>
2022-05-25 18:49:30 +0700

    router created with 4 path home,login,register,and notFound

--- client-newstrsh/src/router/index.js
+++ client-newstrsh/src/router/index.js
@@ -1,5 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import LoginPage from '@/views/LoginPage.vue';
+import RegisterPage from '@/views/RegisterPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +9,32 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: '/login',
+      name: 'login',
+      component: LoginPage,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterPage,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('@/views/NotFoundPage.vue'),
+    },
+  ],
+});
 
-export default router
+// router.beforeEach((to, from, next) => {
+//   const isAuthenticated = localStorage.getItem('access_token');
+//   if (to.name === 'home' && !isAuthenticated) next({ name: 'login' });
+//   else if (to.name === 'login' && isAuthenticated) next({ name: 'home' });
+//   else if (to.name === 'register' && isAuthenticated) next({ name: 'home' });
+//   else next();
+// });
+
+export default router;
