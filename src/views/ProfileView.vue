110bda0f43ee4fca4e00bf53bf887d1900e513a6
Audy-Wallace <audywallace@DESKTOP-UL64QLD.localdomain>
2022-05-25 20:18:48 +0700

    import navbar to profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,32 +1,18 @@
 <script>
 import AddForm from "../components/addForm.vue";
+import NavBar from "../components/NavBar.vue";
 export default {
   name: "ProfileView",
   components: {
     AddForm,
+    NavBar
   },
   methods: {},
 };
 </script>
 <template>
-  <div
-    class="w-[100%] bg-[#f2f2f2] min-h-[100vh] h-max min-w-[1240px] flex justify-center"
-  >
-    <div class="min-h-[100vh] h-max w-[1240px]">
-      <div class="w-[100%] h-[400px]flex flex-col">
-        <div class="navbar text-white bg-black/30 font-jakarta">
-          <div class="flex-1">
-            <a class="btn btn-ghost normal-case text-xl">Stella</a>
-          </div>
-          <div class="flex-none">
-            <ul class="menu menu-horizontal p-0">
-              <li><RouterLink to="/profile">Profile</RouterLink></li>
-              <li><RouterLink to="/register">Logout</RouterLink></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+  <div class="w-[100%] bg-[#f2f2f2] min-h-[100vh] h-max min-w-[1240px] flex justify-center">
+    <NavBar />
     <AddForm />
-    </div>
   </div>
 </template>
