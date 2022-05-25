384d3a0660fe66a43f98f186954a1b13c07da5a8
fikarsuwardi <fikarsuwardi@gmail.com>
2022-05-25 23:13:27 +0800

    add caraousel for upcoming movie

--- movieBagus/src/components/front/Home.vue
+++ movieBagus/src/components/front/Home.vue
@@ -1,16 +1,19 @@
 <script>
 import PopularMovies from "./PopularMovies.vue";
+import UpcomingMovies from "./UpcomingMovies.vue";
 
 export default {
   components: {
     PopularMovies,
-  }
+    UpcomingMovies
+}
 };
 </script>
 
 <template>
 <div class="container mx-auto">
     <PopularMovies />
+    <UpcomingMovies />
   </div>
 </template>
 
