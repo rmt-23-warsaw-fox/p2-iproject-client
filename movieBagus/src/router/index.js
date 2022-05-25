c800597f8c9d543b479fc47c431aaeca4ff104a4
fikarsuwardi <fikarsuwardi@gmail.com>
2022-05-25 17:52:09 +0800

    create detail actor page

--- movieBagus/src/router/index.js
+++ movieBagus/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from "../components/front/Home.vue"
 import Movie from "../components/movies/Movie.vue"
 import Actors from "../components/actors/Actors.vue"
+import ActorDetail from "../components/actors/ActorDetail.vue"
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: "/actors",
       name: "actors",
       component: Actors,
+    },
+    {
+      path: "/actor/:id",
+      name: "actor",
+      component: ActorDetail,
     },
   ]
 })
