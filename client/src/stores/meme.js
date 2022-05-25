f32ad80e4c832096ae23a46ec2e9ae47553f10f0
redyRenno <redyrenno@gmail.com>
2022-05-25 16:50:15 +0900

    fetch third api from server

--- client/src/stores/meme.js
+++ client/src/stores/meme.js
@@ -1,16 +1,28 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
 export const useMemeStore = defineStore({
   id: 'meme',
   state: () => ({
-    counter: 0
+    counter: 0,
+    baseUrl: 'http://localhost:3000',
+    memes: []
   }),
   getters: {
     doubleCount: (state) => state.counter * 2
   },
   actions: {
-    increment() {
-      this.counter++
+    fetchMeme(){
+      axios({
+        method: 'get',
+        url: `${this.baseUrl}/memes`
+      })
+        .then((response) => {
+          this.memes = response.data.memes
+        })
+        .catch((err) => {
+          console.log(err.response);
+        })
     }
   }
 })
--- client/src/views/HomePage.vue
+++ client/src/views/HomePage.vue
@@ -1,12 +1,28 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink } from 'vue-router'
+import { useMemeStore } from '../stores/meme'
+import { mapState, mapActions } from 'pinia'
+
+export default{
+  computed: {
+    ...mapState(useMemeStore, ['memes'])
+  },
+  methods: {
+    ...mapActions(useMemeStore, ['fetchMeme']),
+    localFetchMeme(){
+      }
+  },
+  created(){
+    this.fetchMeme()
+  }
+}
 </script>
 
 <template>
   <div class="full-screen-height bg-white">
     <div class="container container-center">
       <div>
-        <div class="row mt-4">
+        <div class="row my-4">
           <div class="col d-grid gap-3">
             <div class="card  width-400">
               <h6 class="card-header">Faldo - 24 minute ago</h6>
@@ -27,11 +43,38 @@ import { RouterLink, RouterView } from 'vue-router'
                 <a href="#" class="btn btn-primary">Like</a>
               </div>
             </div>
+            <div class="card width-400">
+              <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="https://qph.fs.quoracdn.net/main-qimg-c7a176cb5bcaec3f681b2b69b2e6f158-lq" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="https://i.pinimg.com/736x/ac/b7/f9/acb7f99ba0ef473e03ee81e91a6281ee.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div v-for="meme in memes"  v-bind:key="meme.id" class="carousel-item">
+                    <img v-bind:src="meme.image" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                  data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon bg-dark" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                  data-bs-slide="next">
+                  <span class="carousel-control-next-icon bg-dark" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+            </div>
           </div>
           <div class="col border">
             Chat
             <ul class="list-group">
-              <li class="list-group-item"> <RouterLink to="/chat" href="" class="text-decoration-none">Faldo</RouterLink> </li>
+              <li class="list-group-item">
+                <RouterLink to="/chat" href="" class="text-decoration-none">Faldo</RouterLink>
+              </li>
             </ul>
           </div>
         </div>
