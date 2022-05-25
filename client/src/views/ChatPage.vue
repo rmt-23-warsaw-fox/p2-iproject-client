1046f855a54eebfc11337b7927724e5313a547e0
redyRenno <redyrenno@gmail.com>
2022-05-25 16:16:40 +0900

    develop realtime chat with firebase

--- client/src/views/ChatPage.vue
+++ client/src/views/ChatPage.vue
@@ -1,4 +1,35 @@
 <script>
+import { useChatFirebaseStore } from '../stores/chatFirebase.js'
+import { mapActions, mapState } from 'pinia'
+
+export default {
+  data() {
+    return {
+      message: null,
+      email: null
+    }
+  },
+  computed: {
+    ...mapState(useChatFirebaseStore, ['chats'])
+  },
+  methods: {
+    ...mapActions(useChatFirebaseStore, ['sendMessage', 'fetchMessage']),
+    localSendMessage() {
+      this.sendMessage({
+        senderReceiver: "pablo@email.com",
+        senderEmail: "ago@email.com",
+        message: this.message,
+        createdAt: new Date(),
+      });
+      this.message = null
+    }
+  },
+  created() {
+    this.fetchMessage()
+    this.email = localStorage.getItem('email')
+    // console.log(localStorage.getItem('email'));
+  }
+}
 </script>
 
 <template>
@@ -9,26 +40,22 @@
       </div>
       <div class="card-body">
         <div class="border bg-light mb-4 p-2 rounded-3 d-grid gap-2">
-          <div class="container-left">
+          <div v-for="chat in chats" v-bind:key="chat.id"
+            v-bind:class="email === chat.senderEmail ? 'container-right' : 'container-left'">
             <div class="card-text width-200 border p-2 rounded-3 bg-white">
-              <span>Hai tampan.</span>
-            </div>
-          </div>
-          <div class="container-right">
-            
-            <div class="card-text width-200 border p-2 rounded-3 bg-white">
-              <span>Bacot.</span>
+              <span>{{ chat.message }}</span>
             </div>
           </div>
         </div>
-        <div class="mb-2">
-          <form action="">
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"
-              placeholder="Text here..."></textarea>
+        <div class="">
+          <form v-on:submit.prevent="localSendMessage" action="">
+            <textarea v-model="message" class="form-control" id="exampleFormControlTextarea1" rows="2"
+              placeholder="Text here...">
+            </textarea>
+            <div class="container-right mt-2">
+              <button type="submit" href="#" class="btn btn-primary">Send</button>
+            </div>
           </form>
-        </div>
-        <div class="container-right">
-          <a href="#" class="btn btn-primary">Send</a>
         </div>
       </div>
     </div>
