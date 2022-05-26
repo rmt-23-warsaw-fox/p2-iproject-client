e5edd02948a15a67200f33a305d7b12553ebe07d
Audy-Wallace <audywallace@DESKTOP-UL64QLD.localdomain>
2022-05-26 23:11:51 +0700

    firebase deploy

--- src/api/mainApi.js
+++ src/api/mainApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 const mainAxios = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://ancient-eyrie-19634.herokuapp.com"
 })
 export default mainAxios;
